c1d245c66264cb729b1584ebd17268f0969a900e
woong <ckehfqkr302@naver.com>
2024-06-10 14:19:55 +0900

    다른 사람들의 정답

--- 백준/깊이우선탐색,너비우선탐색/1303)전쟁-전투.js
+++ 백준/깊이우선탐색,너비우선탐색/1303)전쟁-전투.js
@@ -45,3 +45,51 @@ for (i = 0; i < N; i++) {
 const blueAnswer = blueArea.map(v => v * v).reduce((ac, cu) => ac + cu, 0);
 const whiteAnswer = whiteArea.map(v => v * v).reduce((ac, cu) => ac + cu, 0);
 console.log(whiteAnswer, blueAnswer);
+
+// const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+// const [N,M] = input.shift().split(' ').map(v => +v);
+// const graph = Array.from({length: M}).map(() => []);
+// for(let i=0; i<M; i++){
+//     graph[i] = input[i].split('')
+// }
+// const visited = Array.from({length: M}).map(() => new Array(N).fill(false));
+
+// const dx = [-1,1,0,0];
+// const dy = [0,0,-1,1];
+
+// let tempW = 0;
+// let tempB = 0;
+// function dfs(y, x, soldier){
+//    visited[y][x] = true;
+//    if(soldier === 'W') tempW++;
+//    else if(soldier === 'B') tempB++;
+
+//     for(let i=0; i<4; i++){
+//         let nx = x + dx[i];
+//         let ny = y + dy[i];
+//         if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+//         if(!visited[ny][nx] && graph[ny][nx] === soldier){
+//                  dfs(ny, nx, soldier)
+//         }
+//     }
+// }
+
+// let white = 0;
+// let blue = 0;
+
+// for(let i=0; i<M; i++){
+//     for(let j=0; j<N; j++){
+//         if(!visited[i][j]){
+//             if(graph[i][j] === 'W'){
+//                 dfs(i,j, 'W')
+//                 white += (tempW* tempW)
+//                 tempW = 0;
+//             }else if(graph[i][j] === 'B'){
+//                 dfs(i,j, 'B')
+//                 blue += (tempB * tempB)
+//                 tempB = 0;
+//                      }
+//         }
+//     }
+// }
+// console.log(white, blue)
