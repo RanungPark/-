e9e3a5056bec6efa011fe63fbd3b2eae320c503d
woong <ckehfqkr302@naver.com>
2024-08-11 19:34:52 +0900

    잃어버린 괄호

---
+++ baekjoon/그리디/1541)잃어버린 괄호.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const input = fs.readFileSync('../input.txt').toString().trim();
+
+const fomula = input.split('-');
+
+let answer = [];
+
+fomula.forEach(item => {
+  if (item.includes('+')) {
+    let sum = 0;
+    item = item.split('+').map(Number);
+    item.forEach(i => (sum += i));
+    answer.push(sum);
+  } else {
+    answer.push(Number(item));
+  }
+});
+
+console.log(answer.reduce((prev, cur) => prev - cur));
