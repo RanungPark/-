f72b094c7acd791f3007104f6bfbe82440d3179e
woong <ckehfqkr302@naver.com>
2024-09-25 23:07:13 +0900

    더 맵게

---
+++ programmers/2레벨/더 맵게(힙).js
@@ -0,0 +1,110 @@
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  size() {
+    return this.heap.length;
+  }
+
+  getParentIndex(i) {
+    return Math.floor((i - 1) / 2);
+  }
+
+  getLeftChildIndex(i) {
+    return 2 * i + 1;
+  }
+
+  getRightChildIndex(i) {
+    return 2 * i + 2;
+  }
+
+  swap(i, j) {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  }
+
+  peek() {
+    return this.heap[0];
+  }
+
+  insert(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    let parentIndex = this.getParentIndex(index);
+
+    while (index > 0 && this.heap[parentIndex] > this.heap[index]) {
+      this.swap(parentIndex, index);
+      index = parentIndex;
+      parentIndex = this.getParentIndex(index);
+    }
+  }
+
+  remove() {
+    if (this.heap.length === 1) return this.heap.pop();
+    const minValue = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this.heapifyDown();
+    return minValue;
+  }
+
+  heapifyDown() {
+    let index = 0;
+    const length = this.heap.length;
+    const element = this.heap[0];
+
+    while (true) {
+      let leftChildIndex = this.getLeftChildIndex(index);
+      let rightChildIndex = this.getRightChildIndex(index);
+      let leftChild, rightChild;
+      let swapIndex = null;
+
+      if (leftChildIndex < length) {
+        leftChild = this.heap[leftChildIndex];
+        if (leftChild < element) {
+          swapIndex = leftChildIndex;
+        }
+      }
+
+      if (rightChildIndex < length) {
+        rightChild = this.heap[rightChildIndex];
+        if (
+          (swapIndex === null && rightChild < element) ||
+          (swapIndex !== null && rightChild < leftChild)
+        ) {
+          swapIndex = rightChildIndex;
+        }
+      }
+
+      if (swapIndex === null) break;
+      this.swap(index, swapIndex);
+      index = swapIndex;
+    }
+  }
+}
+
+function solution(scoville, K) {
+  const heap = new MinHeap();
+  scoville.forEach(v => heap.insert(v));
+
+  let mixedCount = 0;
+
+  if (heap.peek() >= K) {
+    return mixedCount;
+  }
+
+  while (heap.size() >= 2 && heap.peek() < K) {
+    const first = heap.remove();
+    const second = heap.remove();
+
+    heap.insert(first + second * 2);
+    mixedCount++;
+  }
+
+  return heap.peek() >= K ? mixedCount : -1;
+}
+
+console.log(solution([1, 2, 3, 9, 10, 12], 7));
