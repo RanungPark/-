0cd50444e9a9ca8bd5dbf6662718d94a4edacad7
woong <ckehfqkr302@naver.com>
2024-09-26 23:38:01 +0900

    베스트 앨범 실습

--- programmers/해시 테이블_베스트 앨범 실습.js
+++ programmers/해시 테이블_베스트 앨범 실습.js
@@ -9,13 +9,35 @@ function solution(genres, plays) {
         total: data.total + play,
         songs: [...data.songs, { play, index }]
           .sort((a, b) => b.play - a.play)
-          .slice(0, 2)
-      })
-    })
+          .slice(0, 2),
+      });
+    });
 
   return [...genreMap.entries()]
     .sort((a, b) => b[1].total - a[1].total)
     .flatMap(item => item[1].songs)
-    .map(song => song.index)
+    .map(song => song.index);
+}
 
+function solution(genres, plays) {
+  const genersObj = {};
+
+  genres
+    .map((v, i) => [v, plays[i]])
+    .forEach(([genre, play], index) => {
+      const data = genersObj[genre] || { total: 0, songs: [] };
+      genersObj[genre] = {
+        total: data.total + play,
+        songs: [...data.songs, { play, index }]
+          .sort((a, b) => b.play - a.play)
+          .slice(0, 2),
+      };
+    });
+
+  return Object.entries(genersObj)
+    .sort((a, b) => b[1].total - a[1].total)
+    .reduce(
+      (prev, curr) => prev.concat(curr[1]['songs'].map(v => v['index'])),
+      []
+    );
 }
