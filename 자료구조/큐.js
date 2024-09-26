a839d930929da96d5effc00443d861644e62e13a
woong <ckehfqkr302@naver.com>
2024-09-26 23:38:28 +0900

    큐

---
+++ 자료구조/큐.js
@@ -0,0 +1,68 @@
+class QueueA {
+  constructor() {
+    this.queue = [];
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(value) {
+    this.queue[this.rear] = value;
+    this.rear += 1;
+  }
+
+  dequeue() {
+    const value = this.queue[this.front];
+    delete this.queue[this.front];
+    this.front += 1;
+    return value;
+  }
+
+  peek() {
+    return this.queue[this.front];
+  }
+
+  queueSize() {
+    return this.rear - this.front;
+  }
+}
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class QueueB {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  enqueue(newValue) {
+    const newNode = new Node(newValue);
+    if (this.head === null) {
+      this.head = this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.size += 1;
+  }
+
+  dequeue() {
+    const value = this.head.value;
+    this.head = this.head.next;
+    this.size -= 1;
+    return value;
+  }
+
+  peek() {
+    return this.head.value;
+  }
+
+  queueSize() {
+    return this.size;
+  }
+}
